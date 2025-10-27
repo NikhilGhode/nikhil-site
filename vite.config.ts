d9e6fb2729c32c2324b6db455b9aa7812097efea
NickHill23 <62370072+NickHill23@users.noreply.github.com>
2025-10-27 17:00:12 +0530

    base removed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nikhil-site/",
+  //base: "/nikhil-site/",
   server: {
     host: "::",
     port: 8080,
